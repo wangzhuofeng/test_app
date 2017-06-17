60b7c634f07a5fa9098b0344c95a4cfdd6bb321c
Mark's Test App <mark@getmorebizonline.com>
2017-06-17 04:06:29 +0000

    get rid of extra code

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  
+  before_action :set_article, only: [:edit, :show, :destroy, :update]
   def index
     @articles = Article.all
   end
@@ -8,7 +10,6 @@ class ArticlesController < ApplicationController
   end
   
   def edit
-    @article = Article.find(params[:id])
   end
   
   def create
@@ -23,8 +24,6 @@ class ArticlesController < ApplicationController
   end
   
   def update
-    @article = Article.find(params[:id])
-    
     if @article.update(article_params)
       flash[:notice] = "Article was successfully updated!"
       redirect_to article_path(@article)
@@ -34,17 +33,18 @@ class ArticlesController < ApplicationController
   end
   
   def show
-    @article = Article.find(params[:id])
   end
   
   def destroy
-    @article = Article.find(params[:id])
     @article.delete
     flash[:msg] = "Article deleted!"
     redirect_to articles_path
   end
   
   private
+  def set_article
+     @article = Article.find(params[:id])
+  end
   def article_params
     params.require(:article).permit(:title, :description)
   end
