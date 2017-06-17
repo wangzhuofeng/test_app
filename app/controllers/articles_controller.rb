92e827b44ddaa0e18a2fa3aa6fc17cb1c4b3e65e
Mark's Test App <mark@getmorebizonline.com>
2017-06-17 03:11:10 +0000

    index page, links

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,14 @@
 class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+  
   def new
     @article = Article.new
+  end
+  
+  def edit
+    @article = Article.find(params[:id])
   end
   
   def create
@@ -8,6 +16,17 @@ class ArticlesController < ApplicationController
     
     if @article.save
       flash[:notice] = "Article was successfully created!"
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
+  end
+  
+  def update
+    @article = Article.find(params[:id])
+    
+    if @article.update(article_params)
+      flash[:notice] = "Article was successfully updated!"
       redirect_to article_path(@article)
     else
       render 'new'
