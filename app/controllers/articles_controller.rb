0899b0d8f76827f712edd8ec1d5b6a730f1e7a11
Mark's Test App <mark@getmorebizonline.com>
2017-06-19 03:11:31 +0000

    add association and degugger

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,7 +14,7 @@ class ArticlesController < ApplicationController
   
   def create
     @article = Article.new(article_params)
-    
+    @article.user = User.last
     if @article.save
       flash[:success] = "Article was successfully created!"
       redirect_to article_path(@article)
@@ -24,6 +24,7 @@ class ArticlesController < ApplicationController
   end
   
   def update
+    @article.user = User.last
     if @article.update(article_params)
       flash[:success] = "Article was successfully updated!"
       redirect_to article_path(@article)
