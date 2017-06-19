fa95dd08b6b8796bb36aefa43f719f97c2694d3b
Mark's Test App <mark@getmorebizonline.com>
2017-06-19 02:19:58 +0000

    add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ActiveRecord::Base
+  validates :username, 
+            presence: true, 
+            length: { in:3..25 }, 
+            uniqueness: { case_sensitive: false }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+  validates :email,
+            presence: true,
+            length: { maximum: 100 },
+            format: { with: VALID_EMAIL_REGEX }
 end
