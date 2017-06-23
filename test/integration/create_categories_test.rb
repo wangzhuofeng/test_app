c2d16d41505a68424466da516953d190aab695ac
Mark's Test App <mark@getmorebizonline.com>
2017-06-23 00:51:16 +0000

    add category listing test

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -10,4 +10,15 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
    assert_template 'categories/index'
    assert_match 'sports', response.body
   end
+  
+  test "create invalid category and not successful" do
+   get new_category_path
+   assert_template 'categories/new'
+   assert_no_difference 'Category.count' do
+     post_via_redirect categories_path, category: {name: " "}
+   end
+   assert_template 'categories/new'
+   assert_select 'h2.panel-title'
+   assert_select 'div.panel-body'
+  end
 end
