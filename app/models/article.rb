261aecac35473e599d8cd6895a82e15b0edbeeab
Mark's Test App <mark@getmorebizonline.com>
2017-06-23 06:10:05 +0000

    add article and category association

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,9 +1,8 @@
 class Article < ActiveRecord::Base
   belongs_to :user
-  has_and_belongs_to_many :categories
+  has_many :article_categories
+  has_many :categories, through: :article_categories
   validates :title, presence: true, length: { minimum: 3, maximum: 50 }
   validates :description, presence: true, length: { minimum: 10, maximum: 500 }
   validates :user_id, presence: true
-
-
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,6 @@
 class Category < ActiveRecord::Base
-  has_and_belongs_to_many :articles
+  has_many :article_categories
+  has_many :articles, through: :article_categories
   validates :name, presence: true, uniqueness: true, length: { in: 3..25 }
     
 end
